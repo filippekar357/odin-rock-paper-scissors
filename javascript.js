9dd5680adda450ce1d6a1ade76ed67b966dd4c51
Filip Pekar <pekarf321@gmail.com>
2025-04-23 16:32:30 +0200

    Add functionality to play 1 round of game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice()
 {
@@ -15,5 +17,27 @@ function getHumanChoice()
     return prompt("Rock, paper or scissors?");
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice)
+{
+    if (humanChoice === computerChoice)
+    {
+        console.log("It's a tie!");
+    }
+    else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper"))
+    {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    else
+    {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+}
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
